0a83961297a50b6f5d7ddccb316b3cccb043347c
MahFelix <marcelo12543@hotmail.com>
2023-06-30 21:56:18 -0300

    🧱Alteração de Texto Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
       <navbar className='justify-center items-center bg-[#140B40]'>
         <ul className="flex gap-[119px] text-4xl text-[#9E9E9E] ">
-          <li className="text-[#9E9E9E]"> HOME </li>
+          <li className="text-[#9E9E9E]"> INÍCIO </li>
           <li className="text-[#9E9E9E]"> HISTORIAS </li>
           <li className="text-[#9E9E9E]"> CRIADORES</li>
           <li className="text-[#9E9E9E]"> ARTES </li>
