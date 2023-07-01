3d4be13a0f85045601ad47b685578b441e93b924
MahFelix <marcelo12543@hotmail.com>
2023-06-30 21:43:09 -0300

    🎉 Commit inicial - CC Universe

---
+++ src/App.jsx
@@ -0,0 +1,150 @@
+import DetalheLeft from './assets/DetailsName/DetalheLeft.svg'
+import DetalheRight from './assets/DetailsName/DetalheRight.svg'
+
+
+function App() {
+
+
+
+
+  return (
+    <body  className="flex-col  justify-center">
+      <div className="flex justify-center mt-[30px] mb-[30px]">
+          <h1 className='text-[white] text-7xl justify-center items-center text-[bold]' > Character Chronicle Universe </h1>
+      </div>
+
+      <navbar className='justify-center items-center bg-[#140B40]'>
+        <ul className="flex gap-[119px] text-4xl text-[#9E9E9E] ">
+          <li className="text-[#9E9E9E]"> HOME </li>
+          <li className="text-[#9E9E9E]"> HISTORIAS </li>
+          <li className="text-[#9E9E9E]"> CRIADORES</li>
+          <li className="text-[#9E9E9E]"> ARTES </li>
+          <li className="text-[#9E9E9E]"> CONTATO </li>
+        </ul>
+      </navbar>
+
+      <div className="flex justify-center mt-[50px] mb-[50px]">
+        <h2 className='text-[white] text-5xl justify-center items-center '> Personagens </h2>
+      </div>
+
+      <section className='flex gap-[30px] justify-center items-center'>
+
+          <div id='Backimg-1' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex relative w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40 ' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Ethan Nova </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+          <div id='Backimg-2' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex  w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40 ' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Aveline Morganroth </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+          <div id='Backimg-3' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex  w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40 ' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Lucas Backwood </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+          <div id='Backimg-4' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex  w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40  opacity-1' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Sophia Tompson </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+
+          <div id='Backimg-5' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex  w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40 ' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Alexandre Knight </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+
+
+      </section>
+
+
+      <section className='flex gap-[30px] mt-[30px] justify-center items-center'>
+
+          <div id='Backimg-6' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex  w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40 ' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Noah Summers </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+          <div id='Backimg-7' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex  w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40 ' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Gabriel Nova </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+          <div id='Backimg-8' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex  w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40 ' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Adrian Collins </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+          <div id='Backimg-9' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex  w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40  opacity-1' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Lucas Blackthorn </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+
+          <div id='Backimg-10' className='w-[300px] h-[300px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative'>
+
+            <div className=' flex  w-[300px] bg-[#280430] mt-[81%] rounded-[12px] justify-center bg-opacity-40 ' >
+              <img src={DetalheLeft} />
+                <h2 className=' text-[white] text-1xl p-[10px] mt-[10px] '><strong> Devan Stone </strong></h2>
+              <img src={DetalheRight} />
+            </div>
+
+          </div>
+
+
+
+      </section>
+
+    </body>
+
+  )
+}
+
+export default App
