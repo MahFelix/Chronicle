c5350fd3b9f733e88474481c22885c4ac428f64c
MahFelix <marcelo12543@hotmail.com>
2023-07-09 22:45:03 -0300

    🧱 ci: Modificaçao na responsividade

--- src/Containers/Home/App.jsx
+++ src/Containers/Home/App.jsx
@@ -13,16 +13,16 @@ function App() {
 }
 
   return (
-    <body  className="flex-col  justify-center w-[100%] h-[100%] lg:h-[96.90vh] ">
+    <body  className="flex-col  justify-center w-[100%] h-[100%] lg:h-[96.90vh] max-lg:pb-[200px]">
 
 
       <div className="flex justify-center mt-[30px] mb-[30px] max-lg:flex">
           <h1 className='text-[white] text-7xl justify-center items-center max-lg:ml-6 text-[bold]  max-lg:text-3xl  max-lg:absolute max-lg:hidden' > Character Chronicle Caelum</h1>
-          <h1 className=' hidden text-[white] text-7xl justify-center items-center max-lg:ml-6 text-[bold]  max-lg:text-3xl  max-lg:absolute max-lg:flex' > Character <br/>  Chronicle<br/> Caelum</h1>
+          <h1 className=' hidden text-[white] text-7xl justify-center items-center  text-[bold]  max-lg:text-3xl  max-lg:absolute max-lg:flex ' > Character <br/>  Chronicle<br/> Caelum</h1>
 
 
 <nav>
-            <div className=" flex absolute right-0 lg:hidden max-lg:mr-1 ">
+            <div className=" flex absolute right-0 lg:hidden max-lg:mr-1  ">
         <button onClick={toggleNavbar}>
        {true ? (
        <HamburgerIcon />
@@ -36,12 +36,13 @@ function App() {
          <div id='navbar' className=" justify-end items-start  flex lg:hidden">
 
          <div className="">
-          <ul className="flex flex-row  justify-start mt-[100px] gap-[20px] text-2xl  text-[#9E9E9E]  max-lg:text-sm max-lg:mt-200">
-                  <li className=" w-full p-4 border-b  hover:bg-black  text-[#9E9E9E]"> INÍCIO </li>
-                  <li className=" w-full p-4 border-b  hover:bg-black text-[#9E9E9E]"> HISTORIAS </li>
-                  <li className=" w-full p-4 border-b  hover:bg-black text-[#9E9E9E]"> CRIADORES</li>
-                  <li className=" w-full p-4 border-b  hover:bg-black text-[#9E9E9E]"> ARTES </li>
-                  <li className="  p-4 border-b  hover:bg-black text-[#9E9E9E]"> CONTATO </li>
+          <ul className="flex flex-col justify-start mt-[100px] gap-[10px] text-2xl  text-[#9E9E9E]  max-lg:text-sm max-lg:mt-200 max-lg:absolute max-lg:right-[15px]
+          max-lg:top-0 ">
+                  <li className=" w-full p-1 border-b  hover:bg-black  text-[#9E9E9E]"> INÍCIO </li>
+                  <li className=" w-full p-1 border-b  hover:bg-black text-[#9E9E9E]"> HISTORIAS </li>
+                  <li className=" w-full p-1 border-b  hover:bg-black text-[#9E9E9E]"> CRIADORES</li>
+                  <li className=" w-full p-1 border-b  hover:bg-black text-[#9E9E9E]"> ARTES </li>
+                  <li className="  p-1 border-b  hover:bg-black text-[#9E9E9E]"> CONTATO </li>
         </ul>
         </div>
   </div>
@@ -52,8 +53,8 @@ function App() {
 
     <Navbar />
 
-      <div className="flex justify-center mt-[50px] mb-[50px]">
-        <h2 className='text-[white] text-5xl justify-center items-center max-lg:text-3xl max-lg:mt-[50px]'> Personagens </h2>
+      <div className="flex justify-center mt-[10px] mb-[40px]">
+        <h2 className='text-[white] text-5xl justify-center items-center max-lg:text-3xl max-lg:relative max-lg:top-[150px]'> Personagens </h2>
 
       </div>
 
--- src/components/Cards/Cardprops.jsx
+++ src/components/Cards/Cardprops.jsx
@@ -14,7 +14,7 @@ const Cardprops = (props) => {
 
   return (
     // eslint-disable-next-line react/prop-types
-    <div className=' w-[250px] h-[250px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative max-lg:w-[150px] max-lg:h-[150px]' id={props.id}  onClick={() => handleClick()} >
+    <div className=' w-[250px] h-[250px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative max-lg:w-[150px] max-lg:h-[150px] max-lg:relative  max-lg:top-[150px] ' id={props.id}  onClick={() => handleClick()} >
 
 
           <div className=' flex relative w-[250px] bg-[#280430] mt-[78%] rounded-[12px] justify-around bg-opacity-40 max-lg:w-[150px]  max-lg:text-xs  max-lg:mt-[60%] ' >
