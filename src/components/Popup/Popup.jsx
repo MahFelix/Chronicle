625fcf2f27c8aa70aad178838ba35c6a1d077a1b
MahFelix <marcelo12543@hotmail.com>
2023-07-09 21:55:47 -0300

    🧱 ci: Modificaçao no Popup

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -6,7 +6,8 @@ const Popup = (props) => {
   return (
     // eslint-disable-next-line react/prop-types
     <div  style={{display: props.mostrar ? 'flex' : 'none'}} className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white rounded-lg shadow-lg p-20 transform transition w-[700px] h-[800px] duration-500 hover:scale-110 max-h-500px overflow-y-auto ">
+      <div className="bg-white rounded-lg shadow-lg p-20 transform transition w-[700px] h-[800px] duration-500 hover:scale-110 max-h-500px overflow-y-auto  max-lg:p-8
+       max-lg:w-[500px] max-lg:h-[700px] ">
         <h1 className="text-xl font-bold mb-2">{props.name}</h1>
         <p>{props.text}</p>
         <button onClick={() => props.setValue(!props.mostrar)}
