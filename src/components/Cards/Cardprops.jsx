ff029e7fd93722189d4365093b28e0d56dfdad34
MahFelix <marcelo12543@hotmail.com>
2023-07-13 19:34:42 -0300

    🧪 test: Criando novo teste de Cards

--- src/components/Cards/Cardprops.jsx
+++ src/components/Cards/Cardprops.jsx
@@ -14,13 +14,13 @@ const Cardprops = (props) => {
 
   return (
     // eslint-disable-next-line react/prop-types
-    <div className=' w-[250px] h-[250px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative max-lg:w-[150px] max-lg:h-[150px] max-lg:relative  max-lg:top-[150px] ' id={props.id}  onClick={() => handleClick()} >
+    <div className=' w-[250px] h-[250px] rounded-[12px] opacity-[0.5] transform hover:-translate-y-1 hover:scale-110 transition duration-500 ease-in-out hover:opacity-[1] relative max-lg:w-[100px] max-lg:h-[100px] max-lg:relative  max-lg:top-[100px] ' id={props.id}  onClick={() => handleClick()} >
 
 
-          <div className=' flex relative w-[250px] bg-[#280430] mt-[78%] rounded-[12px] justify-around bg-opacity-40 max-lg:w-[150px]  max-lg:text-xs  max-lg:mt-[60%] ' >
-              <img className='w-[25px]' src={DetalheLeft} />
-              <h1 className=' text-[white] text-1xl p-[10px] mt-[10px] '>{props.name}</h1>
-              <img className='w-[25px]' src={DetalheRight} />
+          <div className=' flex relative w-[250px] bg-[#280430] mt-[78%] rounded-[12px] justify-around bg-opacity-40 max-lg:w-[100px] max-lg:text-xs  max-lg:mt-[55%] ' >
+              <img className='w-[25px] max-lg:w-[10px] ' src={DetalheLeft} />
+              <h1 className=' text-[white] text-1xl p-[10px] mt-[10px] max-lg:text-xs  max-lg:mt-[2px] max-lg:p-[5px]'>{props.name}</h1>
+              <img className='w-[25px] max-lg:w-[10px] ' src={DetalheRight} />
             </div>
 
     </div>
